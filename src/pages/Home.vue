1af10a0598eedf260b8ba3c0cae2fb7d1c1fd18d
Grit-dw <1597329074@qq.com>
2018-08-26 00:56:34 +0800

    添加webpack.base.config中alis 别名、添加iconfont图标等

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
-      Home
+      <home-header></home-header>
+      <router-view></router-view>
     </div>
 </template>
-<script type="text/ecmascript-6"></script>
+<script type="text/ecmascript-6">
+  import HomeHeader from './components/Header.vue'
+  export default {
+    components: {
+      HomeHeader
+    }
+  }
+</script>
 <style></style>
---
+++ src/pages/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="header">
+      <div class="header-left"><i class="iconfont back-icon">&#xe624;</i></div>
+      <div class="header-input"><i class="iconfont search-icon">&#xe632;</i>输入关键字</div>
+      <div class="header-right">城市 <i class="iconfont down-arrow">&#xe62d;</i></div>
+    </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    name: 'HomeHeader'
+  }
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/variables.styl"
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      text-align: center
+      .back-icon
+        font-size: .4rem
+    .header-input
+      flex: 1
+      background: #fff;
+      margin-top: .12rem
+      height: .64rem
+      line-height: .64rem
+      margin-left: .2rem
+      border-radius: .1rem
+      color: #ccc
+      .search-icon
+        margin: 0 .1rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .down-arrow
+        font-size: .2rem
+</style>
