3ac79d7fb688366169a55504c230f2d6aec90669
Grit-dw <1597329074@qq.com>
2018-08-26 22:41:11 +0800

    首页菜单组件

---
+++ src/pages/components/GridMenu.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="menu">
+    <div class="menublock">
+      <swiper>
+        <swiper-slide v-for="(page, index) of pages" :key="index">
+          <div class="menu-icon" v-for="item of page" :key="item.id">
+            <div class="icon-img">
+              <img class="icon-img-content" :src="item.imgUrl" alt="">
+            </div>
+            <p class="icon-desc">{{item.desc}}</p>
+          </div>
+        </swiper-slide>
+      </swiper>
+      <!--<div class="menu-icon">-->
+        <!--<div class="icon-img">-->
+          <!--<i class="icon-img-content iconfont">&#xe602;</i>-->
+        <!--</div>-->
+        <!--<p class="icon-desc">泡温泉</p>-->
+      <!--</div>-->
+      <!--<div class="menu-icon">-->
+        <!--<div class="icon-img">-->
+          <!--<i class="icon-img-content iconfont">&#xe6b9;</i>-->
+        <!--</div>-->
+        <!--<p class="icon-desc">动植物园</p>-->
+      <!--</div>-->
+      <!--<div class="menu-icon">-->
+        <!--<div class="icon-img">-->
+          <!--<i class="icon-img-content iconfont">&#xe605;</i>-->
+        <!--</div>-->
+        <!--<p class="icon-desc">一日游</p>-->
+      <!--</div>-->
+      <!--<div class="menu-icon">-->
+        <!--<div class="icon-img">-->
+          <!--<i class="icon-img-content iconfont">&#xe658;</i>-->
+        <!--</div>-->
+        <!--<p class="icon-desc">必游榜单</p>-->
+      <!--</div>-->
+      <!--<div class="menu-icon">-->
+        <!--<div class="icon-img">-->
+          <!--<i class="icon-img-content iconfont">&#xe69c;</i>-->
+        <!--</div>-->
+        <!--<p class="icon-desc">自然风光</p>-->
+      <!--</div>-->
+      <!--<div class="menu-icon">-->
+        <!--<div class="icon-img">-->
+          <!--<i class="icon-img-content iconfont">&#xe650;</i>-->
+        <!--</div>-->
+        <!--<p class="icon-desc">全部</p>-->
+      <!--</div>-->
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    name: 'HomeIcons',
+    data () {
+      return {
+        iconList: [
+          {id: '0001',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票'
+          },
+          {id: '0002',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+            desc: '滑雪季'
+          },
+          {id: '0003',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+            desc: '泡温泉'
+          },
+          {id: '0004',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+            desc: '动植物园'
+          },
+          {id: '0005',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+            desc: '一日游'
+          },
+          {id: '0006',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png',
+            desc: '必游榜单'
+          },
+          {id: '0007',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/7f/b1ea3c8c7fb6db02.png',
+            desc: '自然风光'
+          },
+          {id: '0008',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/17/4bd370f3eb1acd02.png',
+            desc: '演出'
+          },
+          {id: '0009',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png',
+            desc: '游乐园'
+          }
+        ]
+      }
+    },
+    computed: {
+      pages() {
+        const pages = []
+        this.iconList.forEach((item, index) => {
+          const page = Math.floor(index / 8)
+          if (!pages[page]) {
+            pages[page] = []
+          }
+          pages[page].push(item)
+        })
+        return pages
+      }
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  @import '~styles/mixins.styl'
+  .menu >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .menu
+    margin-top: .1rem
+    .menublock
+      overflow: hidden
+      height: 0
+      padding-bottom: 50%
+      .menu-icon
+        position: relative
+        overflow: hidden
+        float: left
+        width: 25%
+        height: 0
+        padding-bottom: 25%
+        .icon-img
+          position: absolute
+          top: 0
+          left: 0
+          right: 0
+          bottom: .44rem
+          box-sizing: border-box
+          padding: .1rem
+          .icon-img-content
+            height: 100%
+            display: block
+            margin: 0 auto
+        .icon-desc
+          position: absolute
+          left: 0
+          right: 0
+          bottom: 0
+          height: .44rem
+          line-height: .44rem
+          color: $darkText
+          text-align: center
+          ellipsis()
+</style>
