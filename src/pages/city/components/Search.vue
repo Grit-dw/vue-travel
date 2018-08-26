3da1328e2da6bdb58eec4aa40a46005d1d0f1725
Grit-dw <1597329074@qq.com>
2018-08-27 01:20:05 +0800

    城市选择组件中搜索栏实现

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="search">
+      <input type="text" class="search-input" placeholder="输入城市名或拼音">
+    </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    name: 'CitySearch'
+  }
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .search
+    height: .72rem
+    background: $bgColor
+    padding: 0 .1rem
+    .search-input
+      height: .62rem
+      width: 100%
+      box-sizing: border-box
+      padding: 0 .1rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+</style>
