a8a942c7affe911102c2d9fe9794d22050b33565
Grit-dw <1597329074@qq.com>
2018-08-27 00:37:48 +0800

    城市选择组件

---
+++ src/pages/city/City.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <city-header></city-header>
+    </div>
+</template>
+<script type="text/ecmascript-6">
+  import CityHeader from './components/CityHeader'
+  export default {
+    name: 'City',
+    components: {
+      CityHeader
+    }
+  }
+</script>
+<style lang="stylus" scoped></style>
